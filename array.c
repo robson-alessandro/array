feb83d57f340972514e8fde13f0935e230af234a
robson alessandro <117524085+robson-alessandro@users.noreply.github.com>
2022-11-05 17:38:45 -0300

    Update array.c

--- array.c
+++ array.c
@@ -1,2 +1,41 @@
+#include <stdio.h>
+
+int main(){
+    int tamanho;
+    printf("############criar uma lista de numeros sem repetir#########\n");
+    printf("digite o tamanho da lista :");
+    scanf("%d", &tamanho);
+
+    int lista[tamanho];
+    int numero;
+    int posicao = 0;
+
+    do{
+        printf("digite o numero:");
+        scanf("%d" , &numero);
+        int achou=0;
+        
+        for (int i=0 ; i<=posicao; i++){
+
+            if (lista[i]== numero){
+                printf("este numero ja esta na lista\n");
+                achou=1;
+                break;
+            }
+        }
+        if(!achou){
+            lista[posicao] = numero;
+            posicao++;
+        }
+
+        }while(tamanho>posicao);
+    
+
+    for(int j=0 ; j<tamanho; j++){
+            printf("%d",lista[j]);
+    }
+  return 0;
+}
+
 
 
